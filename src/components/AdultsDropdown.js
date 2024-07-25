63252ed528a1f3d687054b5c9488e2574685c398
JOSEBARZOLA <jose_barzola89@hotmail.com>
2024-07-25 20:37:43 -0300

    setTotal adults + kids

--- src/components/AdultsDropdown.js
+++ src/components/AdultsDropdown.js
@@ -14,16 +14,23 @@ const lis = [
 ];
 
 const AdultsDropdown = () => {
+  const {adults, setAdults} = useContext(RoomContext);
+
   return ( <Menu as='div' className='w-full h-full bg-white relative'>
     {/* btn */}
-    <Menu.Button className='w-full h-full flex item-center justify-between px-8'>Adults
-    <BsChevronDown className='text-base text-accent-hover'/>
+    <Menu.Button className='w-full h-full flex items-center justify-between px-8'>{adults}
+    <BsChevronDown className='text-base flex item-center text-accent-hover'/>
     </Menu.Button>
     {/* items */}
-<Menu.Items as='ul' className='bg-white absolute w-full flex flex-col z-40'>
+<Menu.Items as='ul' className='bg-white absolute  w-full flex flex-col z-40'>
+
     {lis.map((li, index)=> {
-    return ( <Menu.Item as='li' className='border-b last-of-type:border-b-0 h-12 hover:bg-accent 
-    hover:text-white w-full flex justify-center item-center cursor-pointer' key={index}>
+    return ( 
+    <Menu.Item 
+    onClick={()=>setAdults(li.name)}
+
+    as='li' className='border-b last-of-type:border-b-0 h-12 hover:bg-accent 
+    hover:text-white w-full flex justify-center items-center cursor-pointer' key={index}>
       
 
       {li.name}
--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -1,13 +1,21 @@
 import React, {createContext, useEffect, useState} from 'react';
 //data
 import{roomData} from '../data';
-
+//create context
 export const RoomContext = createContext()
 
 const RoomProvider = ({children}) => {
   const [rooms, setRooms] = useState(roomData);
+  const [adults, setAdults] = useState('1 Adult');
+  const [kids, setKids] = useState('0 Kids');
+  const [total, setTotal] = useState(0);
+  
+  useEffect(()=>{
+setTotal(Number(adults[0]) + Number(kids[0]));
+  });
+  console.log(total);
   return (
-  <RoomContext.Provider value={{rooms}}>{children}</RoomContext.Provider>);
+  <RoomContext.Provider value={{rooms, adults, setAdults, kids, setKids}}>{children}</RoomContext.Provider>);
 };
 
 export default RoomProvider;
