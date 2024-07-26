6628b01d48a51c8b18b350e287a5b9baf5b5088b
JOSEBARZOLA <jose_barzola89@hotmail.com>
2024-07-26 18:11:16 -0300

    create slider

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -6,6 +6,7 @@ import 'swiper/css';
 import 'swiper/css/effect-fade';
 //import required modules
 import { EffectFade, Autoplay } from 'swiper';
+
 //images
 import Img1 from '../assets/img/heroSlider/1.jpg';
 import Img2 from '../assets/img/heroSlider/2.jpg';
@@ -14,25 +15,58 @@ const slides = [
   {
     title: 'Your luxury hotel for Vacation',
     bg: Img1,
-    btnText: 'Room & Suites',
+    btnText: 'See our rooms',
   },
   {
     title: 'Your luxury hotel for Vacation',
     bg: Img2,
-    btnText: 'Room & Suites',
+    btnText: 'See our rooms',
   },
   {
     title: 'Your luxury hotel for Vacation',
     bg: Img3,
-    btnText: 'Room & Suites',
+    btnText: 'See our rooms',
   }
 ];
 
 const HeroSlider = () => {
   return (
-<Swiper className='heroSlider h-[600px] lg:h-[860px]' >
+<Swiper 
+modules={[EffectFade, Autoplay]} 
+effect={'fade'} 
+loop={true}
+autoplay={{
+  delay: 3000,
+  disableOnInteraction: false,
+}}
+
+className='heroSlider h-[600px] lg:h-[860px]'>
+
   {slides.map((slide, index) =>{
-  return <SwiperSlide>slide</SwiperSlide>
+  //destructure slide
+  const {title, bg, btnText} = slide;
+
+
+  return  <SwiperSlide className='h-full bg-pink-400 relative flex justify-center items-center' key={index}>
+    <div className='z-20 text-white text-center'>
+      <div className='uppercase font-tertiary tracking-[6px] mb-5'>
+        Just Enjoy and relax
+      </div>
+      <h1 className='text-[32px] font-primary uppercase tracking-[2px] max-w-[920px] lg:text-[68px] leading-tight mb-6'>{title}</h1>
+    <button className='btn btn-lg btn-primary mx-auto'>{btnText}</button>
+    </div>
+    <div className='absolute top-0 w-full h-full'>
+    <img className='object-cover h-full w-full'
+      src={bg} 
+      alt=''
+      />
+    </div>
+{/* overlay */}
+<div className='absolute w-full h-full bg-black/70'>
+
+</div>
+
+  </SwiperSlide>
   })}
 </Swiper>
   );
