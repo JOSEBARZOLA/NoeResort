4dfb1356429f912bb8125163ace3b0deeec0cfaa
JOSEBARZOLA <jose_barzola89@hotmail.com>
2024-07-26 17:29:03 -0300

    create navbar & Slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,9 +16,9 @@ const Header = () => {
    className={`${header ? 'bg-white py-6 shadow-lg' : 'bg-transparent py-8'
   } fixed z-50 w-full transition-all duration-500`}
   >
-    <div className='container mx-auto'>
+    <div className='container mx-auto flex  flex-col items-center gap-y-6 lg:flex-row lg:justify-between lg:gap-y-0'>
 {/* logo */}
-<a href='/'>
+<a >
 {header ? (
 <img className='w-[160px]' src={LogoDark} />)
 : 
@@ -27,7 +27,14 @@ const Header = () => {
 </a>
 {/*nav */}
 <nav className={`${header ? 'text-primary' : ' text-white'
-  }`}>nav</nav>
+  } flex gap-x-4 font-tertiary tracking-[3px] text-[15px] items-center uppercase lg:gap-x-8`}>
+    <a href='/' className='hover:text-accent transition'>Home</a>
+    <a href='/' className='hover:text-accent transition'>Rooms</a>
+    <a href='/' className='hover:text-accent transition'>Restaurant</a>
+    <a href='/' className='hover:text-accent transition'>Spa</a>
+    <a href='/' className='hover:text-accent transition'>Contact</a>
+  
+  </nav>
 
   
     </div>
--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,7 +1,41 @@
 import React from 'react';
+//import swiper react component
+import { Swiper, SwiperSlide } from 'swiper/react';
+//import swiper styles  
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+//import required modules
+import { EffectFade, Autoplay } from 'swiper';
+//images
+import Img1 from '../assets/img/heroSlider/1.jpg';
+import Img2 from '../assets/img/heroSlider/2.jpg';
+import Img3 from '../assets/img/heroSlider/3.jpg';
+const slides = [
+  {
+    title: 'Your luxury hotel for Vacation',
+    bg: Img1,
+    btnText: 'Room & Suites',
+  },
+  {
+    title: 'Your luxury hotel for Vacation',
+    bg: Img2,
+    btnText: 'Room & Suites',
+  },
+  {
+    title: 'Your luxury hotel for Vacation',
+    bg: Img3,
+    btnText: 'Room & Suites',
+  }
+];
 
 const HeroSlider = () => {
-  return <div className='heroSlider h-[600px] lg:h-[860px]  bg-black  '>HeroSlider</div>;
+  return (
+<Swiper className='heroSlider h-[600px] lg:h-[860px]' >
+  {slides.map((slide, index) =>{
+  return <SwiperSlide>slide</SwiperSlide>
+  })}
+</Swiper>
+  );
 };
 
 export default HeroSlider;
