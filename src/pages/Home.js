8ef9795fe77aef626514ed1027b631c9a04f6ccc
JOSEBARZOLA <jose_barzola89@hotmail.com>
2024-10-01 21:17:10 -0300

    retire bg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
    <>
    <HeroSlider/>
    <div className='container mx-auto relative'>
-    <div className='bg-pink-400/20 mt-4 p-4 lg:shadow-2xl lg:absolute lg:left-0 lg:right-0 lg:p-0 lg:z-30'>
+    <div className=' mt-4 p-4 lg:shadow-2xl lg:absolute lg:left-0 lg:right-0 lg:p-0 lg:z-30'>
     {/* <BookForm /> */} {/*quitar llave para descomentar  */}
     </div>
    </div>
